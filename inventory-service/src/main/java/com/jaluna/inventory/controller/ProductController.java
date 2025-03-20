7348f0f46ff8776f0417fc712d571db4bcc81226
Jhonn Andersson Luna Acosta <jhonandersonluna02@gmail.com>
2025-03-20 08:17:13 -0500

    Microservicio de Inventario implementado

---
+++ inventory-service/src/main/java/com/jaluna/inventory/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.jaluna.inventory.controller;
+
+import com.jaluna.inventory.model.Product;
+import com.jaluna.inventory.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/inventory")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/products")
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
